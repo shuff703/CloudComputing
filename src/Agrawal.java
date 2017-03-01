d5ee7e32fe7c99eaa8eb9c82a3468c9f30149924
Scott Huff <shuff703@gmail.com>
2017-03-01 13:56:39 -0500

    Added mapper and combiner classes

--- src/Agrawal.java
+++ src/Agrawal.java
@@ -15,42 +15,51 @@ public class Agrawal {
 			e.printStackTrace();
 		}
 		
-		String[] input = new String[6];
+		String[] input = new String[5];
 		int i = 0;
 		while(fr.hasNextLine()){
 			
 			input[i] = fr.nextLine();
+			System.out.println(input[i]);
+			i++;
 			
 		}
 		
+		apriori(2, 50, input);
+		
 	}
 	
-	private void apriori(int minSupport, float confidence, String[] input){
+	private static void apriori(int minSupport, float confidence, String[] input){
 		
 		HashMap<String, Integer> nodes = new HashMap<>();
 		
 		for(int i = 0; i<input.length; i++){
 			
-			input[i].substring(1, input[i].length()-2);
+			input[i] = input[i].replaceAll("([()\t])", "");
 			String [] items = input[i].split(", ");
 			
 			for(int x = 0; x<items.length; x++){
 				
-				if(nodes.get(items[x]) == null){
-					nodes.put(items[x], 1);
+				//System.out.println(items[x].hashCode());
+				if(nodes.containsKey(items[x])){
+					nodes.replace(items[x], nodes.get(items[x])+1);
 				}
 				else{
-					nodes.replace(items[x], nodes.get(items[x])+1);
+					nodes.put(items[x], 1);
 				}
 			}
 			
 		}
 		
-		for(String key : nodes.keySet()){
-			
+		for(String key : ((HashMap<String, Integer>) nodes.clone()).keySet()){
+			System.out.println(key);
 			if(nodes.get(key) < minSupport){	
 				nodes.remove(key);
+				for(int i = 0; i<input.length; i++){
+					input[i].replaceAll(key, "");
+				}
 			}
+			
 		}
 		
 		
---
+++ src/PatternMapper.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.LongWritable;
+
+public class PatternMapper 
+	extends Mapper<LongWritable, Text, Text, IntWritable>{
+	
+	@Override
+	public void map(LongWritable key, Text value, Context context) 
+			throws IOException, InterruptedException{
+		String line = value.toString();
+		line.replaceAll("([()\t])", "");
+		ArrayList<String> subsets = new ArrayList<>();
+		String [] values = line.split(",");
+		
+        for (int i = 0; i < (1<<values.length); i++){
+            StringBuilder sb = new StringBuilder();
+ 
+            // Print current subset
+            for (int j = 0; j < values.length; j++){
+ 
+                if ((i & (1 << j)) > 0){
+                	sb.append(values[j]+",");
+                }
+            subsets.add(sb.toString());
+            }
+        }
+        
+        for(String set: subsets){
+        	
+        	context.write(new Text(set), new IntWritable(1));
+        }
+
+	}
+
+}
